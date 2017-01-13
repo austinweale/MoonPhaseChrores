615b280ee17ad2ce0d5f906daa7ede6876fb804b
Austin Weale <austinweale@gmail.com>
2017-01-13 09:57:42 -0800

    starting to add periodic tweeting

--- chores.py
+++ chores.py
@@ -16,10 +16,24 @@ tweetbot = MarkovBot()
 # Get the current directory's path
 dirname = os.path.dirname(os.path.abspath(__file__))
 # Construct the path to the book
-book = os.path.join(dirname, 'Freud_Dream_Psychology.txt')
+book = os.path.join(dirname, 'phase-chores.txt')
 # Make your bot read the book!
 tweetbot.read(book)
 
 my_first_text = tweetbot.generate_text(25, seedword=['dream', 'psychoanalysis'])
 print("tweetbot says:")
 print(my_first_text)
+
+Python
+
+# Log in to Twitter
+tweetbot.twitter_login(cons_key, cons_secret, access_token, access_token_secret)
+
+Python
+
+# Start periodically tweeting
+tweetbot.twitter_tweeting_start(days=0, hours=19, minutes=30, keywords=None, prefix=None, suffix='#PyGaze')
+
+# Use the following to stop periodically tweeting
+# (Don't do this directly after starting it, or your bot will do nothing!)
+#tweetbot.twitter_tweeting_stop()
