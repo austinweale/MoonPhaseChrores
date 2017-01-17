8824985786e499917371a442472e001db8bd344c
Austin Weale <austinweale@gmail.com>
2017-01-16 17:49:08 -0800

    using node

---
+++ chores.js
@@ -0,0 +1,41 @@
+var twit = require('twit');
+var config = require('./config.js');
+var schedule = require('node-schedule');
+
+
+var CronJob = require('node-cron');
+
+var Twitter = new twit(config);
+
+var phases = ["fckn test",
+				"observe the MOON, and rotate the WHEEL. it's a half moon.", 
+				"it's a new moon, babu!", 
+				"First quarter fingers crossed for no wet hole!"]
+
+var num = 0
+CronJob.schedule('0 18 19, 27, 3 * *', function() {
+	console.log("test");
+		if(num < phases.length){
+		Twitter.post('statuses/update', { status: phases[num]}, function(err, data, response) {
+	  		console.log(data)
+		})
+		num++;
+	}	
+  //console.log('The answer to life, the universe, and everything!');
+  
+});
+
+/*T.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
+  console.log(data)
+})*/
+/*
+*    *    *    *    *    *
+┬    ┬    ┬    ┬    ┬    ┬
+│    │    │    │    │    |
+│    │    │    │    │    └ day of week (0 - 7) (0 or 7 is Sun)
+│    │    │    │    └───── month (1 - 12)
+│    │    │    └────────── day of month (1 - 31)
+│    │    └─────────────── hour (0 - 23)
+│    └──────────────────── minute (0 - 59)
+└───────────────────────── second (0 - 59, OPTIONAL) 
+*/
---
+++ config.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+  consumer_key: 'eW7wk6JukwBShbsaIW3eChugO',  
+  consumer_secret: 'ERBXaB2r4X1Ab9ONCZwqxiVx4r70aiSR0QXhiVeMp8mX5bZHJm',
+  access_token: '819717437805371400-bvV0j8Rr1FULUmDLyvyfae6Csr3DEGX',  
+  access_token_secret: 'eydOBkLUkxvtCYJWfsFC9kheoPSxDfI1ysHxgbvMkABLD'
+}
